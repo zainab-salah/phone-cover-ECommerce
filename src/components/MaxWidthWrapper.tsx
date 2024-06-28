2b5a4637e2e49a87494992f989aa5b6e29a9910e
Kainy Onodera <kainykawai@gmail.com>
2024-06-28 20:07:12 +0300

    first commit

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+
+type MaxWidthWrapperProps = {
+  className?: string;
+  children?: React.ReactNode;
+};
+
+const MaxWidthWrapper = ({ className, children }: MaxWidthWrapperProps) => {
+  return (
+    <div
+      className={cn(
+        "h-full mx-auto container max-w-screen-lg px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
