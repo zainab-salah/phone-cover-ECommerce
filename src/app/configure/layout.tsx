f8a4f7eae565667be5fc8e48a030b1143f82e02c
Kainy <kainykawai@gmail.com>
2024-09-25 15:09:10 +0300

    added stepper for uploading

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,11 +1,15 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import Steps from "@/components/Steps"
 
  
 const Layout = ({children}:{
     children: React.ReactNode
 }) => {
   return (
-    <MaxWidthWrapper className="flex-1 flex flex-col">{children}</MaxWidthWrapper>
+    <MaxWidthWrapper className="flex-1 flex flex-col">
+      <Steps />
+
+      {children}</MaxWidthWrapper>
   )
 }
 
