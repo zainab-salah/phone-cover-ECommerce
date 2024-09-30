e34fe252f632b11c57bdbaab9c8f3b4e5bed065a
Kainy <kainykawai@gmail.com>
2024-09-30 19:54:49 +0300

    Feat: made an email template for sucess orders

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,76 +1,103 @@
-import { db } from "@/db";
-import { stripe } from "@/lib/stripe";
-import { ok } from "assert";
-import { headers } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-import Stripe from "stripe";
+import { db } from '@/db'
+import { stripe } from '@/lib/stripe'
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+import Stripe from 'stripe'
+import { Resend } from 'resend'
+import OrderReceivedEmail from '@/components/emails/OrderReceivedEmail'
 
-export async function POST(req: NextRequest) {
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function POST(req: Request) {
   try {
-    const body = await req.text();
-    const signature = headers().get("stripe-signature");
+    const body = await req.text()
+    const signature = headers().get('stripe-signature')
+
     if (!signature) {
-      return new Response("No signature", { status: 400 });
+      return new Response('Invalid signature', { status: 400 })
     }
 
     const event = stripe.webhooks.constructEvent(
       body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
-    );
+    )
 
-    if (event.type === "checkout.session.completed") {
-      if (event.data.object.customer_details?.email) {
-        throw new Error("Missing user email");
+    if (event.type === 'checkout.session.completed') {
+      if (!event.data.object.customer_details?.email) {
+        throw new Error('Missing user email')
       }
-      const session = event.data.object as Stripe.Checkout.Session;
+
+      const session = event.data.object as Stripe.Checkout.Session
+
       const { userId, orderId } = session.metadata || {
         userId: null,
         orderId: null,
-      };
-      if (!userId || !orderId) {
-        throw new Error("Missing metadata");
       }
-      const billingDetails = session.customer_details!.address;
-      const shippingDetails = session.shipping_details!.address;
-      await db.order.update({
+
+      if (!userId || !orderId) {
+        throw new Error('Invalid request metadata')
+      }
+
+      const billingAddress = session.customer_details!.address
+      const shippingAddress = session.shipping_details!.address
+
+      const updatedOrder = await db.order.update({
         where: {
           id: orderId,
         },
         data: {
           isPaid: true,
-
           shippingAddress: {
             create: {
               name: session.customer_details!.name!,
-              city: shippingDetails!.city!,
-              country: shippingDetails!.country!,
-              postalCode: shippingDetails!.postal_code!,
-              street: shippingDetails!.line1!,
-              state: shippingDetails!.state!,
+              city: shippingAddress!.city!,
+              country: shippingAddress!.country!,
+              postalCode: shippingAddress!.postal_code!,
+              street: shippingAddress!.line1!,
+              state: shippingAddress!.state,
             },
           },
           billingAddress: {
             create: {
               name: session.customer_details!.name!,
-              city: billingDetails!.city!,
-              country: billingDetails!.country!,
-              postalCode: billingDetails!.postal_code!,
-              street: billingDetails!.line1!,
-              state: billingDetails!.state!,
+              city: billingAddress!.city!,
+              country: billingAddress!.country!,
+              postalCode: billingAddress!.postal_code!,
+              street: billingAddress!.line1!,
+              state: billingAddress!.state,
             },
           },
         },
-      });
-    }
-    return NextResponse.json({ result: event, ok: true });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Somthing went wrong", ok: false },
-      { status: 500 }
-    );
+      })
 
-    
+      await resend.emails.send({
+        from: 'CaseCobra <kainyCase@gmail.com>',
+        to: [event.data.object.customer_details.email],
+        subject: 'Thanks for your order!',
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state,
+          },
+        }),
+      })
+    }
+
+    return NextResponse.json({ result: event, ok: true })
+  } catch (err) {
+    console.error(err)
+
+    return NextResponse.json(
+      { message: 'Something went wrong', ok: false },
+      { status: 500 }
+    )
   }
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { buttonVariants } from './ui/button'
-import { ArrowRight } from 'lucide-react' 
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from 'lucide-react'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+
 const Navbar = async () => {
- 
-  const {getUser} =  getKindeServerSession();
-  const user = await getUser();
- const isAdmin =  user?.email === process.env.ADMIN_EMAIL;
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL
 
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
