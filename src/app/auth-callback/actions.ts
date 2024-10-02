71c9c4403ca40a5de5900e34dfe66036ceddb6e5
Kainy <kainykawai@gmail.com>
2024-10-02 20:44:55 +0300

    Kainy remove this crap later

--- src/app/auth-callback/actions.ts
+++ src/app/auth-callback/actions.ts
@@ -14,6 +14,9 @@ export const getAuthStatus = async () => {
   const existingUser = await db.user.findFirst({
     where: { id: user.id },
   })
+
+
+
   
   if (!existingUser) {
     await db.user.create({
